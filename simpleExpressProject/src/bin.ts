fe5eb64bdbe948603f9174e790bde76c4870d674
Waqar ahmed <waqarahmed44870@gmail.com>
2024-11-30 12:48:14 +0500

    commit

---
+++ simpleExpressProject/src/bin.ts
@@ -0,0 +1,5 @@
+import {app} from "./index"
+
+app.listen(8000,  ()=> {
+   console.log("listening on port 8000");
+})
--- simpleExpressProject/src/tests/index.test.ts
+++ simpleExpressProject/src/tests/index.test.ts
@@ -1,32 +1,60 @@
 import {describe, expect,  it} from '@jest/globals';
-import request from "supertest";
 import { app } from "../index"
+import  request  from 'supertest';
+  describe("post /sum", () => {
+    it("shoudl return the sum", async () => {
+      const res = await request(app).post("/sum").send({
+        a: 1,
+        b: 2
+      })
+      expect(res.statusCode).toBe(200);
+      expect(res.body.answer).toBe(3);
+    })
+    it("shoudl return the sum of negative numbers", async () => {
+      const res = await request(app).post("/sum").send({
+        a: -1,
+        b: -2
+      })
+      expect(res.statusCode).toBe(200);
+      expect(res.body.answer).toBe(-3);
+    })
 
-describe("POST /sum", () => {
-    it("should return the sum of two numbers", async () => {
-        const res = await request(app).post("/sum").send({
-          a: 1,
-          b: 2
-        });
-        expect(res.statusCode).toBe(200);
-        expect(res.body.answer).toBe(3);
-      });
+    it("should retrun the two zero numbers", async () => {
+      const res = await request(app).post("/sum").send({
+        a: 0,
+        b: 0
+      })
+      expect(res.statusCode).toBe(200);
+      expect(res.body.answer).toBe(0);
+    })
 
-      it("should return the sum of two negative numbers", async () => {
-        const res = await request(app).post("/sum").send({
-          a: -1,
-          b: -2
-        });
-        expect(res.statusCode).toBe(200);
-        expect(res.body.answer).toBe(-3);
-      });
+  })
 
-      it("should return the sum of two zero number", async () => {
-        const res = await request(app).post("/sum").send({
-          a: 0,
-          b: 0
-        });
-        expect(res.statusCode).toBe(200);
-        expect(res.body.answer).toBe(0);
-      });
-});
+// describe("POST /sum", () => {
+//     it("should return the sum of two numbers", async () => {
+//         const res = await request(app).post("/sum").send({
+//           a: 1,
+//           b: 2
+//         });
+//         expect(res.statusCode).toBe(200);
+//         expect(res.body.answer).toBe(3);
+//       });
+
+//       it("should return the sum of two negative numbers", async () => {
+//         const res = await request(app).post("/sum").send({
+//           a: -1,
+//           b: -2
+//         });
+//         expect(res.statusCode).toBe(200);
+//         expect(res.body.answer).toBe(-3);
+//       });
+
+//       it("should return the sum of two zero number", async () => {
+//         const res = await request(app).post("/sum").send({
+//           a: 0,
+//           b: 0
+//         });
+//         expect(res.statusCode).toBe(200);
+//         expect(res.body.answer).toBe(0);
+//       });
+// });
