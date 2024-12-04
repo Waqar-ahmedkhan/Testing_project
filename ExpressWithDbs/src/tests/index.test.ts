4474a5958edf7f8a10bc094fa47fc1820f3b6108
Waqar ahmed <waqarahmed44870@gmail.com>
2024-12-04 21:34:11 +0500

    commit

--- ExpressWithDbs/src/tests/index.test.ts
+++ ExpressWithDbs/src/tests/index.test.ts
@@ -23,3 +23,28 @@ describe("Post/ sum", () => {
      });
   })
 })
+import { describe, it,  expect } from "vitest";
+import request from "supertest";
+import  {app} from "../index"
+
+
+describe("Post/ sum", () => {
+  it("should be add two positive numbers", () => {
+    return request(app)
+     .post("/sum")
+     .send({ a: 5, b: 10 })
+     .expect(200)
+     .then((response) => {
+       expect(response.body.sum).toBe(15);
+     });
+  })
+  it("should be two negative numbers", () => {
+    return request(app)
+     .post("/sum")
+     .send({ a: -5, b: -10 })
+     .expect(200)
+     .then((response) => {
+       expect(response.body.sum).toBe(-15);
+     });
+  })
+})
